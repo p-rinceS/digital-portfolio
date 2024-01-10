2edd50beb00d194b17ca3a5ad1eba807c113a8d0
PrinceS <princesonani9112@gmail.com>
2024-01-09 19:37:26 -0600

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+
+    <div>
+    <head>
+<title>Prince Sonani</title>
+<meta name= "description"></meta>
+    </head>
+ 
+    <main>
+    
+      <section className='min-h-screen'>
+      <nav>
+        <h1>
+          developed by prince
+          <ul>
+            <li></li>
+
+            <li><a href='#'></a></li>
+          </ul>
+        </h1>
+      </nav>
+      
+      </section>
+   
+    </main>
+
+    </div>
+    
+  )
+}
