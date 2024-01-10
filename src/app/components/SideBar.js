1fd3aa2123c869a64276b8ec2bd4163a9e0f4e91
PrinceS <princesonani9112@gmail.com>
2024-01-09 23:46:05 -0600

    Update SideBar styling and add tooltips

--- src/app/components/SideBar.js
+++ src/app/components/SideBar.js
@@ -4,13 +4,13 @@ import { IoMailSharp } from "react-icons/io5";
 
 const SideBar = () => { 
 return(
-<div className='fixed top-0 left-0 h-screen w-24 flex flex-col bg-primary drop-shadow-lg text-gray-600        '>
+<div className='fixed top-0 left-0 h-screen w-24 flex flex-col  drop-shadow-lg text-gray-600 bg-gradient-to-b from-primary to-glow     '>
 
 {/* add icons as more stuff needs to be added */}
-<SideBarIcon icon={<FaDownload size = "40" />} />
-<SideBarIcon icon={<FaPencil size = "40"/>} />
-<SideBarIcon icon={<FaGithub  size = "40"/>} />
-<SideBarIcon icon={<IoMailSharp   size = "40"/>} />
+<SideBarIcon icon={<FaDownload size = "40" />} text = {"CV"}/>
+<SideBarIcon icon={<FaPencil size = "40"/>} text = {"About"}/>
+<SideBarIcon icon={<FaGithub  size = "40"/>}text = {"GitHub"} />
+<SideBarIcon icon={<IoMailSharp   size = "40"/>}text = {"Contact Me"} />
 
 
 </div>
@@ -19,10 +19,14 @@ return(
 
 
 
-const SideBarIcon = ({icon}) => (
+const SideBarIcon = ({icon, text = "tooltip"}) => (
 
-    <div className="sidebar-icon">   
+    <div className="sidebar-icon group">   
         {icon}
+
+        <span className="sidebar-tooltip  -z-10 group-hover:translate-x-32 group-hover:opacity-100 group-hover:rounded-md ">
+                {text}
+        </span>
     </div>
 
 );
